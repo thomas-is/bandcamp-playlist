694e0e0e906c54e7571bdd59e8d154912e5ba75a
thomas-is <thomas@0l.fi>
2022-09-24 13:26:56 +0200

    :shell: use bin/sh instead of bash as we use Alpine

---
+++ srv/bin/gencovers.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+thumbnails() {
+  cd $@
+  pwd
+  if [ -f "cover.jpg" ] ; then
+    for PX in 64 128 320 480
+    do
+      if [ ! -f "cover-${PX}x.jpg" ] ; then
+        convert -resize ${PX}x${PX} cover.jpg "cover-${PX}px.jpg"
+      fi
+    done
+  fi
+
+  IFS=$'\n'
+  for FILE in $( ls )
+  do
+    if [ -d $FILE ] ; then
+      thumbnails $FILE
+    fi
+  done
+  cd ..
+}
+
+thumbnails $@
+
+
+
