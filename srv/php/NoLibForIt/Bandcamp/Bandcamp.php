24cf255b7569d291b6580a601f1449ab07f69e2e
thomas-is <thomas@0l.fi>
2022-10-01 23:19:59 +0200

    :recycle: refacto Bandcamp, Track (WIP)

---
+++ srv/php/NoLibForIt/Bandcamp/Bandcamp.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace NoLibForIt\Bandcamp;
+
+
+class Bandcamp {
+
+
+  private $html;
+
+  private $artist    ;
+  private $album     ;
+  private $cover_src ;
+  private $released  ;
+  private $url       ;
+
+  private $tracks    ;
+
+  public const DELIMITER = "_";
+
+  public static function safeString( string $s) {
+    return preg_replace("/[^a-zA-Z0-9]/","_",$s);
+  }
+
+  /**
+   * extract $value of the first $key="$value" in $this->html
+   * @param  string  $key
+   * @return string  value
+   */
+  private function valueOf( $key ) {
+    $start = strpos($this->html, "$key=\"") + strlen("$key=\"");
+    $end   = strpos($this->html, '"', $start);
+    return substr($this->html, $start, $end-$start);
+  }
+
+  /**
+   * extract $value of the first $key="$value" in $this->html,
+   * decode html entities and json decode
+   * @param  string  $key
+   * @return array
+   */
+  private function decode( $key ) {
+    return (array) json_decode(
+        html_entity_decode($this->getRawDataValue($key))
+        ,true
+        );
+  }
+
+
+  public function __construct( $url, $maxlen = 400000 ){
+
+    $this->html = @file_get_contents( $url, NULL, NULL, NULL, $maxlen );
+
+    if ( empty($this->html) ) {
+      throw new \Exception("no contents!");
+    }
+
+    $tralbum         = $this->decode("data-tralbum");
+
+    $this->artist    = @$tralbum['artist'];
+    $this->album     = @$tralbum['packages'][0]['album_title'];
+    $this->cover     = $this->valueOf('<meta property="og:image" content');
+    $this->released  = @$tralbum['album_release_date'];
+    $this->url       = @$tralbum['url'];
+
+    $this->tracks = [];
+    foreach ( (array) @$tralbum['trackinfo'] as $trackinfo ) {
+      $this->tracks[] = new Track($trackinfo);
+    }
+
+
+  }
+
+  public function htmlArtist()   { return htmlspecialchars($this->artist);   }
+  public function htmlAlbum()    { return htmlspecialchars($this->album);    }
+  public function htmlReleased() {
+    return $this->released
+      ? date("Y-m-d",strtotime($this->release_date))
+      : "??-??-??";
+  }
+  /*
+   *   FS formated getters
+   */
+  private function fileArtist() {
+    return self::safeString($this->artist);
+  }
+  private function fileAlbum()  {
+    return self::safeString($this->album);
+  }
+  private function fileReleased()  {
+    return $this->released
+      ? date("Ymd",strtotime($this->released))
+      : "00000000";
+  }
+  public function filePrefix() {
+    return $this->fileArtist()
+      . DIRECTORY_SEPARATOR
+      . $this->fileReleased()
+      . Bandcamp::DELIMITER
+      . $this->fileAlbum();
+  }
+  public function fileCover() {
+    return $this->filePrefix()
+      . DIRECTORY_SEPARATOR
+      . "cover.jpg";
+  }
+  public function download(){
+    $d = new DownloadAs($this->cover,$this->fileCover());
+    file_put_contents(
+      self::NFO
+        . $this->filePrefix()
+        . DIRECTORY_SEPARATOR
+        . "nfo.json"
+      , $this->nfo()
+    );
+    foreach($this->tracks as $track) {
+      $url = $track->get_mp3url();
+      $file = $this->get_FS_prefix().$track->get_FS_suffix();
+      $d = new DownloadAs($url,$file);
+    }
+  }
+
+
+
+}
+
+?>
---
+++ srv/php/NoLibForIt/Bandcamp/Track.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace \NoLibForIt\Bandcamp
+
+class Track {
+
+    const DELIMITER = Bandcamp::DELIMITER;
+
+    public $num;
+    public $title;
+    public $duration;
+    public $mp3url;
+
+    public function __construct( array $data ){
+      $this->num       = (int)    @$data['track_num'];
+      $this->title     = (string) @$data['title'];
+      $this->mp3url    = (string) @$data['file']['mp3-128'];
+      $this->duration  = (int)    @$data['duration'];
+    }
+
+    /**
+    *   HTML getters
+    */
+    public function displayNum()      { return sprintf("%02d",$this->num); }
+    public function displayTitle()    { return htmlspecialchars($this->title); }
+    public function displayDuration() { return date("i:s",$this->duration); }
+    /**
+    *   FS getters
+    */
+    public function fileNum()        { return $this->displayNum(); }
+    public function fileTitle()      { return self::safeString($this->title); }
+    public function fileSuffix() {
+        return DIRECTORY_SEPARATOR.$this->fileNum().self::DELIMITER.$this->fileTitle().".mp3";
+    }
+
+
+}
+
